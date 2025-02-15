fb7b0878ca5211a872bb494237c62657b9ce672c
rshm <rshm.varma@gmail.com>
2025-02-15 19:33:29 +0100

    change prize ladder

--- src/data.js
+++ src/data.js
@@ -4969,21 +4969,21 @@ const data =
     ];
 
 const prizeMoney = [
-  { id: 1, amount: "₹ 5000" },
-  { id: 2, amount: "₹ 15000" },
-  { id: 3, amount: "₹ 30000" },
-  { id: 4, amount: "₹ 60000" },
-  { id: 5, amount: "₹ 100000" },
-  { id: 6, amount: "₹ 150000" },
-  { id: 7, amount: "₹ 250000" },
-  { id: 8, amount: "₹ 400000" },
-  { id: 9, amount: "₹ 600000" },
-  { id: 10, amount: "₹ 1000000" },
-  { id: 11, amount: "₹ 5000000" },
-  { id: 12, amount: "₹ 100000000" },
-  { id: 13, amount: "₹ 300000000" },
-  { id: 14, amount: "₹ 500000000" },
-  { id: 15, amount: "₹ 1000000000" }
+  { id: 1, amount: "Suraksha Sadhak (Safety Practitioner)" },
+  { id: 2, amount: "Suraksha Jigyasu (Safety Seeker)" },
+  { id: 3, amount: "Suraksha Pathik (Safety Explorer)" },
+  { id: 4, amount: "Suraksha Upasak (Safety Devotee)" },
+  { id: 5, amount: "Suraksha Samarthak (Safety Supporter)" },
+  { id: 6, amount: "Suraksha Gyaani (Safety Knowledgeable)" },
+  { id: 7, amount: "Suraksha Vishleshak (Safety Analyst)" },
+  { id: 8, amount: "Suraksha Margdarshak (Safety Guide)" },
+  { id: 9, amount: "Suraksha Prabhari (Safety In-Charge)" },
+  { id: 10, amount: "Suraksha Shreshtha (Safety Excellent)" },
+  { id: 11, amount: "Suraksha Mahanayak (Safety Hero)" },
+  { id: 12, amount: "Suraksha Vidwan (Safety Scholar)" },
+  { id: 13, amount: "Suraksha Ratna (Safety Gem)" },
+  { id: 14, amount: "Suraksha Mahaguru (Safety Grand Master)" },
+  { id: 15, amount: "Suraksha Adhipati" }
 ].reverse();
 
 export { prizeMoney, data };
