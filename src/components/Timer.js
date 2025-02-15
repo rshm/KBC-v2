9081ad2c19fcf836ae8ef9411447c8443be217b0
rshm <rshm.varma@gmail.com>
2025-02-15 18:30:00 +0100

    change timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -15,7 +15,7 @@ const Timer = ({ setTimeOut, questionNumber, stopTimer, setStopTimer }) => {
   }, [timer, setTimeOut, stopTimer]);
 
   useEffect(() => {
-    setTimer(30);
+    setTimer(60);
     setStopTimer(false); // Reset stopTimer when questionNumber changes
   }, [questionNumber, setStopTimer]);
 
