4121d1e271ec2f27f03a3a6108974a397f8ed38b
rshm <rshm.varma@gmail.com>
2025-02-16 10:41:43 +0100

    put last 10 seconds timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,12 +1,18 @@
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import countdown from "../sounds/countdown.mp3";
+import useSound from "use-sound";
 
 const Timer = ({ setTimeOut, questionNumber, stopTimer, setStopTimer }) => {
   const [timer, setTimer] = useState(30);
-
+  const [countdownPlay] = useSound(countdown);
   useEffect(() => {
     if (stopTimer) return; // Stop the timer if stopTimer is true
 
-    if (timer === 0) return setTimeOut(true);
+    if (timer === 0) {
+      setTimeOut(true);
+      return;
+    }
+
     const interval = setInterval(() => {
       setTimer((prev) => prev - 1);
     }, 1000);
@@ -17,9 +23,16 @@ const Timer = ({ setTimeOut, questionNumber, stopTimer, setStopTimer }) => {
   useEffect(() => {
     setTimer(60);
     setStopTimer(false); // Reset stopTimer when questionNumber changes
-  }, [questionNumber, setStopTimer]);
+  }, [questionNumber]);
 
-  return timer;
+  // Play countdown sound when timer reaches 10 seconds
+  useEffect(() => {
+    if (timer === 10) {
+      countdownPlay()
+    }
+  }, [timer]);
+
+  return <div>{timer}</div>;
 };
 
 export default Timer;
